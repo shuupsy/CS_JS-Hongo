9bda0df8dae9f336cef2564a1abdb1ea8c1066bc
shuupsy <101631301+shuupsy@users.noreply.github.com>
2022-06-14 16:13:25 +0200

    JS : Produits, titre

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,4 @@
 // Navbar fixed
-let body = document.querySelector("body")
 let nav = document.querySelector("nav")
 let navLinks = document.querySelectorAll(".nav-link")
 let sect1 = document.querySelector("#sect-1").offsetTop
@@ -24,5 +23,49 @@ function navBg() {
             element.classList.remove("text-light")
         })
     }}
-
 window.addEventListener("scroll", navBg)
+
+
+// Produits
+class Produits {
+    constructor(nom, prix, liens) {
+        this.nom = nom;
+        this.prix = prix;
+        this.liens = liens;
+    }
+}
+
+let p1 = new Produits("Textured Sweater", 50.00, "public/img/fashion/fashion-recent-products-01-1-1.jpg")
+let p2 = new Produits("Traveller Shirt", 510.00, "public/img/fashion/fashion-recent-products-02-1-1.jpg")
+let p3 = new Produits("Crewneck Sweatshirt", 50.00, "public/img/fashion/fashion-recent-products-01-b.jpg")
+let p4 = new Produits("Skinny Trousers", 160.00, "public/img/fashion/fashion-recent-products-04.jpg")
+let p5 = new Produits("High Neck Sweater", 35.00, "public/img/fashion/fashion-recent-products-05-1.jpg")
+let p6 = new Produits("Sleeve Sweater", 120.00, "public/img/fashion/fashion-recent-products-06-1.jpg")
+let p7 = new Produits("Pocket Sweatshirt", 410.00, "public/img/fashion/fashion-recent-products-07-1.jpg")
+let p8 = new Produits("Top With Pleated", 50.00, "public/img/fashion/fashion-recent-products-08-1.jpg")
+let p9 = new Produits("Cotton Sweater", 155.00, "public/img/fashion/fashion-recent-products-01-d.jpg")
+let p10 = new Produits("Textured Plain Regular", 70.00, "public/img/fashion/fashion-recent-products-10-1.jpg")
+console.log(p1.prix)
+
+let produits = [p1,p2,p3,p4,p5,p6,p7,p8,p9,p10]
+
+// Ajout du titre
+const sect3 = document.querySelector("#products")
+sect3.style.padding = "15vh 0"
+
+let h2 = document.createElement("h2")
+h2.innerHTML = "Recent Products"
+h2.classList.add("py-2")
+
+let p = document.createElement("p")
+p.innerHTML = "Lorem Ipsum is simpy dummy text of the printing and typesetting industry printing and industry."
+p.style.fontSize = "1rem"
+p.classList.add("text-secondary")
+
+let div = document.createElement("div")
+div.style.padding = "0 35vw"
+div.style.textAlign = "center"
+
+sect3.appendChild(div)
+div.appendChild(h2)
+div.appendChild(p)
