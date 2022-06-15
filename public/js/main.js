feea4a0390dfb2b5a7817198bb4b029145532fdd
shuupsy <101631301+shuupsy@users.noreply.github.com>
2022-06-15 09:58:23 +0200

    JS : Produits level1

--- public/js/main.js
+++ public/js/main.js
@@ -22,32 +22,34 @@ function navBg() {
         navLinks.forEach(element => {
             element.classList.remove("text-light")
         })
-    }}
+    }
+}
 window.addEventListener("scroll", navBg)
 
 
 // Produits
 class Produits {
-    constructor(nom, prix, liens) {
+    constructor(nom, prix, lien) {
         this.nom = nom;
         this.prix = prix;
-        this.liens = liens;
+        this.lien = lien;
     }
 }
 
-let p1 = new Produits("Textured Sweater", 50.00, "public/img/fashion/fashion-recent-products-01-1-1.jpg")
-let p2 = new Produits("Traveller Shirt", 510.00, "public/img/fashion/fashion-recent-products-02-1-1.jpg")
-let p3 = new Produits("Crewneck Sweatshirt", 50.00, "public/img/fashion/fashion-recent-products-01-b.jpg")
-let p4 = new Produits("Skinny Trousers", 160.00, "public/img/fashion/fashion-recent-products-04.jpg")
-let p5 = new Produits("High Neck Sweater", 35.00, "public/img/fashion/fashion-recent-products-05-1.jpg")
-let p6 = new Produits("Sleeve Sweater", 120.00, "public/img/fashion/fashion-recent-products-06-1.jpg")
-let p7 = new Produits("Pocket Sweatshirt", 410.00, "public/img/fashion/fashion-recent-products-07-1.jpg")
-let p8 = new Produits("Top With Pleated", 50.00, "public/img/fashion/fashion-recent-products-08-1.jpg")
-let p9 = new Produits("Cotton Sweater", 155.00, "public/img/fashion/fashion-recent-products-01-d.jpg")
-let p10 = new Produits("Textured Plain Regular", 70.00, "public/img/fashion/fashion-recent-products-10-1.jpg")
+let p1 = new Produits("Textured Sweater", "$50.00", "public/img/fashion/fashion-recent-products-01-1-1.jpg")
+let p2 = new Produits("Traveller Shirt", "$510.00", "public/img/fashion/fashion-recent-products-02-1-1.jpg")
+let p3 = new Produits("Crewneck Sweatshirt", "$50.00", "public/img/fashion/fashion-recent-products-01-b.jpg")
+let p4 = new Produits("Skinny Trousers", "$160.00", "public/img/fashion/fashion-recent-products-04.jpg")
+let p5 = new Produits("High Neck Sweater", "$35.00", "public/img/fashion/fashion-recent-products-05-1.jpg")
+let p6 = new Produits("Sleeve Sweater", "$120.00", "public/img/fashion/fashion-recent-products-06-1.jpg")
+let p7 = new Produits("Pocket Sweatshirt", "$410.00", "public/img/fashion/fashion-recent-products-07-1.jpg")
+let p8 = new Produits("Top With Pleated", "$50.00", "public/img/fashion/fashion-recent-products-08-1.jpg")
+let p9 = new Produits("Cotton Sweater", "$155.00", "public/img/fashion/fashion-recent-products-01-d.jpg")
+let p10 = new Produits("Textured Plain Regular", "$70.00", "public/img/fashion/fashion-recent-products-10-1.jpg")
 console.log(p1.prix)
 
-let produits = [p1,p2,p3,p4,p5,p6,p7,p8,p9,p10]
+let produits = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10]
+
 
 // Ajout du titre
 const sect3 = document.querySelector("#products")
@@ -62,10 +64,47 @@ p.innerHTML = "Lorem Ipsum is simpy dummy text of the printing and typesetting i
 p.style.fontSize = "1rem"
 p.classList.add("text-secondary")
 
-let div = document.createElement("div")
-div.style.padding = "0 35vw"
-div.style.textAlign = "center"
+let divT = document.createElement("div")
+divT.style.padding = "0 35vw"
+divT.style.textAlign = "center"
 
-sect3.appendChild(div)
-div.appendChild(h2)
-div.appendChild(p)
+sect3.appendChild(divT)
+divT.appendChild(h2)
+divT.appendChild(p)
+
+// Boite des items shopping
+let divB = document.createElement("div")
+sect3.appendChild(divB) // Ajout dans la boite
+divB.setAttribute("class", "text-center px-5 my-5")
+divB.style.display = "grid"
+divB.style.gridTemplateColumns = "repeat(auto-fit, minmax(200px,1fr))"
+divB.style.justifyContent = "center"
+divB.style.gap = "10px"
+divB.style.fontSize = "0.8rem"
+
+
+// SUPER LOOP
+produits.forEach(item => {
+    // Div Element qui contient image + la div des p
+    let divE = document.createElement("div")
+    divB.appendChild(divE)
+    // Div Image + Div p
+    let divI = document.createElement("div")
+    divE.appendChild(divI)
+    let divP = document.createElement("div")
+    divE.appendChild(divP)
+    // Image
+    let imgShopping = document.createElement("img")
+    imgShopping.setAttribute("src", item.lien)
+    imgShopping.style.width = "100%"
+    divI.appendChild(imgShopping)
+    // p
+    let nom = document.createElement("p")
+    nom.innerHTML = item.nom
+    nom.classList.add("fw-bold")
+    let prix = document.createElement("p")
+    prix.innerHTML = item.prix
+    prix.classList.add("text-secondary")
+    divP.appendChild(nom)
+    divP.appendChild(prix)
+})
