053d15c700c891004f0e647d9f21428e0437b15f
shuupsy <101631301+shuupsy@users.noreply.github.com>
2022-06-16 16:53:04 +0200

    JS: dark mode, hamburger

--- public/js/modules/nav.js
+++ public/js/modules/nav.js
@@ -3,6 +3,7 @@ let nav = document.querySelector("nav")
 let navLinks = document.querySelectorAll(".nav-link")
 let sect1 = document.querySelector("#sect-1").offsetTop
 let logo = document.querySelector(".logo")
+let hamburger = document.querySelector(".navbar-toggler")
 
 function navBg() {
     let x = window.scrollY
@@ -11,6 +12,7 @@ function navBg() {
         nav.style.backgroundColor = "#222222"
         nav.style.color = "white"
         nav.classList.add("fixed-top")
+        hamburger.classList.add("bg-light")
         navLinks.forEach(element => {
             element.classList.add("text-light")
         })
@@ -19,6 +21,7 @@ function navBg() {
         nav.style.backgroundColor = "unset"
         nav.style.color = "unset"
         nav.classList.remove("fixed-top")
+        hamburger.classList.remove("bg-light")
         navLinks.forEach(element => {
             element.classList.remove("text-light")
         })
