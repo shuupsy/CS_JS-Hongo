be5d55037c4ebccc18fbb38606795b6432398dd7
shuupsy <101631301+shuupsy@users.noreply.github.com>
2022-06-16 16:36:26 +0200

    JS: amélioration dark mode

--- public/js/modules/dark.js
+++ public/js/modules/dark.js
@@ -1,21 +1,37 @@
 // Dark mode 
 let sections = document.querySelectorAll("section")
-
 let dark = document.querySelector(".dark")
+
 dark.addEventListener("click", () => {
-    dark.classList.toggle("moon")
+    dark.classList.toggle("moon") // Changement d'îcone
+    let btnT = document.querySelectorAll(".bg-transparent")
+
     if (dark.classList.contains("moon")) {
         dark.innerHTML = `<i class="fa-solid fa-sun"></i>`
         sections.forEach(e => {
-            e.style.backgroundColor = "black"
+            if (e.classList.contains("darker")) {
+                e.style.backgroundColor = "#111111"
+            } else {
+                e.style.backgroundColor = "#2A2B2E"
+            }
             e.style.color = "white"
+        })
+        btnT.forEach(e => {
+            e.style.color = "white" // Bouton testimonials
         })
     } else {
         dark.classList.remove("moon")
         dark.innerHTML = `<i class="fa-solid fa-moon"></i>`
         sections.forEach(e => {
-            e.style.backgroundColor = "unset"
+            if (e.classList.contains("darker")) {
+                e.style.backgroundColor = "#F7F2ED"
+            } else {
+                e.style.backgroundColor = "unset"
+            }
             e.style.color = "unset"
+        })
+        btnT.forEach(e => {
+            e.style.color = "unset" // Bouton testimonials
         })
     }
 })
